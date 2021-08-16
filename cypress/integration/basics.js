1c47d4a750e04f33a63bb5443b6153a553b2a11c
Marcin <marcinkufel84@gmail.com>
2021-08-16 06:56:24 +0200

    added website

--- cypress/integration/basics.js
+++ cypress/integration/basics.js
@@ -1,32 +1,12 @@
-const { before, after } = require("cypress/types/lodash");
-const { beforeEach } = require("mocha");
+/// <reference types="Cypress"/>
 
 describe("Login feature Test", () => {
 
-    before(() => {
-        cy.log("Run the server locally");
-    })
-
-    after(() => {
-        cy.log("kill the server");
-    })
-
-    beforeEach(() => {
-        cy.log("navigate to login page");
-    })
 
     it("should visit the login page correctly", () => {
 
-        cy.log("website should have the correct url");
-
-    })
-
-    it("should be able to login", () => {
-
-        cy.log("fill the username and the password and login");
-        cy.log("should navigate the login page");
-
-
+        cy.visit("cypress/index.html");
+        
     })
 
  })
