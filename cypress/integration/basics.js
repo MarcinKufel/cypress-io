8d66acb4d26c15fbd90c3c30eaf1ee712c0f49d0
Marcin <marcinkufel84@gmail.com>
2021-08-16 09:07:28 +0200

    added new tests with assertions

--- cypress/integration/basics.js
+++ cypress/integration/basics.js
@@ -6,7 +6,8 @@ describe("Login feature Test", () => {
     it("should visit the login page correctly", () => {
 
         cy.visit("cypress/index.html");
-        
+        cy.get('[name="courses"]').select('wdio').should('contain', 'wdio');
+        cy.get('[type="checkbox"]').check('Fries').should('be.checked');
     })
 
  })
